96b0632cadb3f88fdd7c91486ed4cf6569aa6bbf
KorbsStudio <korbsstudio@github.com>
2021-06-12 03:11:19 -0400

    update

--- installer.sh
+++ installer.sh
@@ -2,11 +2,11 @@ echo "Installing Guilded..."
 
 wget "https://github.com/KorbsStudio/Guilded-for-Linux/releases/download/v0.0.2/guilded.zip"
 unzip guilded.zip
-cd guilded
 sudo cp -R Guilded/ /opt/
 sudo cp -R usr/share/* /usr/share/
 echo "Cleaning..."
 cd
-sudo rm -R guilded
+sudo rm -R usr/
+sudo rm -R Guilded/
 
 echo "Done. Guilded should appear in your application menu soon."
